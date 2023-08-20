7e17e0f61612d613c4cc56c21b6c874d33970100
deltaR <deltar_chen@outlook.com>
2023-08-21 01:35:35 +0800

    update import&default

--- config/default.py
+++ config/default.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 # Server config
-SERVER_NAME = 'c.xjtu.live'
+SERVER_NAME = 'icourse.npuer.life'
 DEBUG = False
 # DEBUG = True
 # SERVER_NAME = 'localhost'
@@ -11,9 +11,9 @@ for arg in sys.argv:
   if arg == '-d':
     DEBUG = True
 
-SECRET_KEY = os.environ['COURSE_SECRET_KEY']
-EMAIL_CONFIRM_SECRET_KEY = os.environ['COURSE_EMAIL_CONFIRM_SECRET_KEY']
-PASSWORD_RESET_SECRET_KEY = os.environ['COURSE_PASSWORD_RESET_SECRET_KEY']
+SECRET_KEY = 'course-secret-key'
+EMAIL_CONFIRM_SECRET_KEY = 'course-email-confirm-secret-key'
+PASSWORD_RESET_SECRET_KEY = 'course-password-reset-secret-key'
 
 # available languages
 LANGUAGES = {
@@ -22,7 +22,7 @@ LANGUAGES = {
 }
 
 # SQL config
-SQLALCHEMY_DATABASE_URI = os.environ['COURSE_SQLALCHEMY_DATABASE_URI']
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://icourse:guadaxuanke@localhost/icourse?charset=utf8mb4'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Flask mail
@@ -33,7 +33,7 @@ MAIL_USE_SSL = False
 MAIL_DEBUG = DEBUG
 MAIL_USERNAME = None
 MAIL_PASSWORD = None
-MAIL_DEFAULT_SENDER = 'course-review-support@xjtu.live'
+MAIL_DEFAULT_SENDER = 'course-review-support@npuer.life'
 MAIL_MAX_EMAILS = None
 # MAIL_SUPPRESS_SEND =
 MAIL_ASCII_ATTACHMENTS = False
@@ -60,10 +60,10 @@ DEBUG_TB_TEMPLATE_EDITOR_ENABLED = True
 DEBUG_TB_INTERCEPT_REDIRECTS = False
 
 # URL to return to after signing on
-RETURN_URL = "https://c.xjtu.live/signincallback/"
+RETURN_URL = "https://icourse.npuer.life/signincallback/"
 # URL to Discourse
-DISCOURSE_URL = "https://xjtu.live"
+DISCOURSE_URL = "https://npuer.life"
 # replace with your own secret
-CALL_DISCOURSE_SSO_SECRET = os.environ['COURSE_DISCOURSE_AS_SSO_PROVIDER_SECRETE']
+CALL_DISCOURSE_SSO_SECRET = 'FtwFnoJpYNHemwR3CimF'
 assert len(CALL_DISCOURSE_SSO_SECRET) > 20
 CALL_DISCOURSE_SSO_SECRET = bytes(CALL_DISCOURSE_SSO_SECRET, 'utf-8')
